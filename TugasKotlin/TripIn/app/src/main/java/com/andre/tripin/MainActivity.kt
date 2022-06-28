0a6e22ca3e66bdd09294f7001819250ad46560a5
andremoore123 <Andremoore431@gmail.com>
2022-06-28 09:54:18 +0700

    Adding Profile and Splash Screen

--- TugasKotlin/TripIn/app/src/main/java/com/andre/tripin/MainActivity.kt
+++ TugasKotlin/TripIn/app/src/main/java/com/andre/tripin/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.andre.tripin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.profile ->{
+                    val intent = Intent(this, Profile::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
--- TugasKotlin/TripIn/app/src/main/java/com/andre/tripin/SplashScreen.kt
+++ TugasKotlin/TripIn/app/src/main/java/com/andre/tripin/SplashScreen.kt
@@ -1,11 +1,37 @@
 package com.andre.tripin
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import java.lang.Boolean.getBoolean
 
+@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+    private val status: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
+        val defaultStatus = sharedPref.getBoolean(getString(R.string.status), status)
+        if (!defaultStatus){
+            with (sharedPref.edit()) {
+                putBoolean(getString(R.string.status), true)
+                commit()
+            }
+            Handler().postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                onDestroy()
+            }, 3000)
+        } else if (defaultStatus){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
