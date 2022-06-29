c9d038ac22de69074b847de8f5c562916442b7ef
andremoore123 <Andremoore431@gmail.com>
2022-06-29 20:23:00 +0700

    Big Update, change color palette, adding fragment for each purposes

--- TugasKotlin/TripIn/app/src/main/java/com/andre/tripin/MainActivity.kt
+++ TugasKotlin/TripIn/app/src/main/java/com/andre/tripin/MainActivity.kt
@@ -3,23 +3,24 @@ package com.andre.tripin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.navOptions
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
-        topAppBar.setOnMenuItemClickListener { menuItem ->
-            when(menuItem.itemId){
-                R.id.profile ->{
-                    val intent = Intent(this, Profile::class.java)
-                    startActivity(intent)
-                    true
-                }
-                else -> false
-            }
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostMainController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            .setupWithNavController(navHostMainController)
     }
 }
---
+++ TugasKotlin/TripIn/app/src/main/java/com/andre/tripin/explore/ExploreFragment.kt
@@ -0,0 +1,17 @@
+package com.andre.tripin.explore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andre.tripin.R
+class ExploreFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+}
